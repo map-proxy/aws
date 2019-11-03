e59381ab3f3a452340f25438e0d9299c52614b28
Duy Phan <ducduymm0@gmail.com>
2019-11-03 10:36:24 +0700

    Updates

---
+++ precache-manifest.e842b4e847ef84322fd972843dd06113.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a20bbfbc14983c483cb39e5d908cd86a",
+    "url": "/aws/index.html"
+  },
+  {
+    "revision": "383e8708e76abcd4d71d",
+    "url": "/aws/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "167cf4e8e2b50253fffd",
+    "url": "/aws/static/js/2.9482f913.chunk.js"
+  },
+  {
+    "revision": "383e8708e76abcd4d71d",
+    "url": "/aws/static/js/main.61d0c151.chunk.js"
+  },
+  {
+    "revision": "c617e95e9a08b8d383e1",
+    "url": "/aws/static/js/runtime~main.c4d9e07b.js"
+  }
+]);
